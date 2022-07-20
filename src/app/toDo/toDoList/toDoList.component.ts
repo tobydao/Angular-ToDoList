0b2a46929eb4227569d7cbb77353337164eef52b
tobydao <tob.dao@gmail.com>
2022-07-20 16:36:54 +0200

    rename todo.model

--- src/app/toDo/toDoList/toDoList.component.ts
+++ src/app/toDo/toDoList/toDoList.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { todo } from '../todo.model'
+
 @Component({
   selector: 'app-toDoList',
   templateUrl: './toDoList.component.html',
 })
 
 export class ToDoListComponent  {
+
+  toDoElements: todo[] = [
+    new todo("vacuum")
+  ]
   
 }
