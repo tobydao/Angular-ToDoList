3c3bb990e79cfaf27e3a08d23ea791f301c7c425
tobydao <tob.dao@gmail.com>
2022-07-22 08:45:25 +0200

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { ToDoElementComponent } from './toDo/toDoElement/toDoElement.component';
 import { ToDoListComponent } from './toDo/toDoList/toDoList.component';
@@ -18,7 +17,6 @@ import { EditComponent } from './edit/edit.component'
   declarations: 
   [ 
     AppComponent,
-    HelloComponent,
     HeaderComponent,
     ToDoElementComponent,
     ToDoListComponent,
