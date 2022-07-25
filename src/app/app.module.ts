1ee1c3601b59a9f9c379d081586701ca5c047b3b
tobydao <tob.dao@gmail.com>
2022-07-25 15:56:39 +0200

    APIService added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,9 @@ import { EditComponent } from './edit/edit.component'
   imports:
   [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
+
   ],
   declarations: 
   [ 
